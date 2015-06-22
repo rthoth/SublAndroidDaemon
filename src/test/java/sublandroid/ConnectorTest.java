d662c652a2c4ca3f27f907e817eae1e2ebf35c29
rthoth <ronaldo.silva@lemaf.ufla.br>
2015-06-22 19:45:11 -0300

    pref fastjson

--- src/test/java/sublandroid/ConnectorTest.java
+++ src/test/java/sublandroid/ConnectorTest.java
@@ -14,33 +14,6 @@ import com.fasterxml.jackson.databind.*;
 
 public class ConnectorTest {
 
-	private static class Runner implements Runnable {
-
-		private final Connector connector;
-		private final Reader reader;
-		private final Writer writer;
-		private final Thread thread;
-
-
-		public Runner(final Connector connector, final Reader reader, final Writer writer) {
-			this.reader = reader;
-			this.writer = writer;
-			this.connector = connector;
-			this.thread = new Thread(this);
-			this.thread.start();
-		}
-
-		public void run() {
-			try {
-				System.out.println("Starting @ " + Thread.currentThread().getName());
-				this.connector.listen(reader, writer);
-			} catch (Throwable throwable) {
-				System.out.println("Ops...");
-				throwable.printStackTrace();
-			}
-		}
-	}
-
 
 	@Test(timeOut=1000)
 	public void loadValidDirectory() throws Exception {
@@ -52,8 +25,6 @@ public class ConnectorTest {
 		final PipedWriter inputWriter = new PipedWriter();
 		final BufferedReader inputReader = new BufferedReader(new PipedReader(inputWriter));
 
-
-		System.out.println("Test @ " + Thread.currentThread().getName());
 		final Runner runner = new Runner(connector, outputReader, inputWriter);
 
 		send(MCommand.from("start"), outputWriter);
--- src/test/java/sublandroid/Util.java
+++ src/test/java/sublandroid/Util.java
@@ -7,6 +7,32 @@ import java.io.*;
 
 public abstract class Util {
 
+	public static class Runner implements Runnable {
+
+		private final Connector connector;
+		private final Reader reader;
+		private final Writer writer;
+		private final Thread thread;
+
+
+		public Runner(final Connector connector, final Reader reader, final Writer writer) {
+			this.reader = reader;
+			this.writer = writer;
+			this.connector = connector;
+			this.thread = new Thread(this);
+			this.thread.start();
+		}
+
+		public void run() {
+			try {
+				this.connector.listen(reader, writer);
+			} catch (Throwable throwable) {
+				throwable.printStackTrace();
+			}
+		}
+	}
+
+
 	public static ObjectMapper OBJECT_MAPPER = new ObjectMapper(Jackson.FACTORY);
 
 	public static void send(MCommand message, Writer writer) throws IOException {
