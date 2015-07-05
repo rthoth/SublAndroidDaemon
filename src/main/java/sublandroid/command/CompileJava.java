f4a952ea53b0d08e7f735e73b7c2637b63b97da2
rthoth <ronaldo.silva@lemaf.ufla.br>
2015-07-05 14:43:21 -0300

    Refactor

--- src/main/java/sublandroid/command/CompileJava.java
+++ src/main/java/sublandroid/command/CompileJava.java
@@ -3,6 +3,7 @@ package sublandroid.command;
 import sublandroid.messages.*;
 
 import java.io.*;
+import java.util.*;
 import java.util.regex.*;
 import org.gradle.tooling.*;
 
@@ -11,15 +12,15 @@ public class CompileJava extends Command {
 	public static final String COMMAND = "compileJava";
 
 
+	protected static final Pattern ERROR_PATTERN = Pattern.compile("^([^:]+):(\\d+):\\s+([^:]+):\\s+(.+)$");
+
 	protected static final String CANNOT_FIND_SYMBOL = "cannot find symbol";
-	protected static final Pattern METHOD_IN_CLASS_PATTERN = Pattern.compile("method\\s+[^\\s]+\\sin\\sclass");
-	protected static final Pattern CONSTRUCTOR_IN_CLASS_PATTERN = Pattern.compile("constructor\\s+[^\\s]+\\sin\\sclass");
 
-	protected static final String NL_PATTERN = "[\r\n]+";
+	protected static final Pattern DETAIL_PATTERN = Pattern.compile("^\\W*([^:]+):\\s+(.+)$");
 
-	protected static final Pattern JAVA_ERROR_PATTERN = Pattern.compile("^([^:]+):(\\d+):\\s*([^:]+):\\s*(.*)$");
+	protected static final String LINE_BREAK = "[\\r\\n]+";
 	
-	protected static final Pattern DETAIL_PATTERN = Pattern.compile("^\\W*([^:]+):\\s*(.+)$");
+	protected static final Pattern SEMANTIC_ERROR = Pattern.compile("^[^\\s]+\\s[^\\s]+\\sin\\sclass");
 
 
 	@Override
@@ -36,10 +37,10 @@ public class CompileJava extends Command {
 
 			final String errOut = new String(context.error.toByteArray());
 
-			final String[] lines = errOut.split(NL_PATTERN);
+			final String[] lines = errOut.split(LINE_BREAK);
 
 			for (int i=0; i<lines.length; i++) {
-				final Matcher matcher = JAVA_ERROR_PATTERN.matcher(lines[i]);
+				final Matcher matcher = ERROR_PATTERN.matcher(lines[i]);
 
 				if (matcher.matches()) {
 					final String fileName = matcher.group(1);
@@ -60,33 +61,31 @@ public class CompileJava extends Command {
 
 						message.addJavaFailure(fileName, lineNumber, kind, what, where, description);
 
-					} else if (METHOD_IN_CLASS_PATTERN.matcher(what).find() || CONSTRUCTOR_IN_CLASS_PATTERN.matcher(what).find()) {
-						final Matcher det1Matcher = DETAIL_PATTERN.matcher(lines[++i]);
-						final Matcher det2Matcher = DETAIL_PATTERN.matcher(lines[++i]);
-						final Matcher det3Matcher = DETAIL_PATTERN.matcher(lines[++i]);
+					} else if (SEMANTIC_ERROR.matcher(what).find()) {
 
-						String lbl1, val1, lbl2, val2, lbl3, val3;
+						LinkedList<String> details = new LinkedList<>();
 
-						lbl1 = lbl2 = lbl3 = val1 = val2 = val3 = "";
-
-						if (det1Matcher.find()) {
-							lbl1 = det1Matcher.group(1);
-							val1 = det1Matcher.group(2);
+						for (Matcher detailMatcher; (i + 1) < lines.length;) {
+							
+							detailMatcher = DETAIL_PATTERN.matcher(lines[++i]);
+							
+							if (detailMatcher.matches()) {
+								details.add(String.format("(%s: %s)", detailMatcher.group(1), detailMatcher.group(2)));
+							} else {
+								--i;
+								break;
+							}
 						}
 
-						if (det2Matcher.find()) {
-							lbl2 = det2Matcher.group(1);
-							val2 = det2Matcher.group(2);
+						final StringBuilder description = new StringBuilder();
+
+						while (!details.isEmpty()) {
+							description.append(details.poll());
+							if (!details.isEmpty())
+								description.append(", ");
 						}
 
-						if (det3Matcher.find()) {
-							lbl3 = det3Matcher.group(1);
-							val3 = det3Matcher.group(2);
-						}
-
-						final String description = String.format("(%s: %s), (%s: %s), (%s: %s)", lbl1, val1, lbl2, val2, lbl3, val3);
-
-						message.addJavaFailure(fileName, lineNumber, kind, what, where, description);
+						message.addJavaFailure(fileName, lineNumber, kind, what, where, description.toString());
 					} else {
 						message.addJavaFailure(fileName, lineNumber, kind, what, where);
 					}
