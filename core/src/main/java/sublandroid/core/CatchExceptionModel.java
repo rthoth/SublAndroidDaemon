eb99e350a8796cf5fba6f33a9e2103eb4fffcbe9
rthoth <ronaldo.silva@lemaf.ufla.br>
2015-08-09 16:57:52 -0300

    new status interface

--- core/src/main/java/sublandroid/core/CatchExceptionModel.java
+++ core/src/main/java/sublandroid/core/CatchExceptionModel.java
@@ -3,5 +3,24 @@ package sublandroid.core;
 import java.util.*;
 
 public interface CatchExceptionModel {
+
+	public interface Status {
+		boolean isActionError();
+		boolean isOk();
+		boolean isUnexpectedValidationError();
+		boolean isValidationError();
+	}
+
+	public Throwable getError();
+
+	public List<String> getErrors();
+
+	public String getFailedTaskName();
+
+	public String getFailedTaskPath();
+
+	public Status getStatus();
+
 	public List<String> getTasks();
+
 }
